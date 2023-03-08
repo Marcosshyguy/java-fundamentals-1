3cd99859f87c708e729f12988f4f0efc5cfb85a1
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-08 16:51:26 +0100

    Add:bonus to FizzBuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,15 +1,27 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++){
-            if (i % 3 == 0){
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }else {
-                System.out.println(i);
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert a number between 1 and 1000: ");
+        int random = Integer.parseInt(scan.nextLine());
+
+        if (random > 0 || random <= 1000){
+            for (int i = 1; i <= random; i++){
+                if (i % 3 == 0){
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }else {
+                    System.out.println(i);
+                }
             }
+        }else {
+            System.out.println("retry");
         }
     }
 }
