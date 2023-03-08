e51af82601d730d2068a278544a52b7ff8df6496
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-08 17:15:00 +0100

    Fix: FizzBuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Insert a number between 1 and 1000: ");
         int random = Integer.parseInt(scan.nextLine());
 
         if (random > 0 || random <= 1000){
             for (int i = 1; i <= random; i++){
-                if (i % 3 == 0){
-                    System.out.println("Fizz");
-                } else if (i % 5 == 0) {
-                    System.out.println("Buzz");
-                }else {
+                if(i % 3 == 0 && i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0 || i % 5 == 0){
+                    if (i % 3 == 0) {
+                        System.out.println("Fizz");
+                    }else if(i % 5 == 0){
+                        System.out.println("Buzz");
+                    }
+                    }else {
                     System.out.println(i);
                 }
+
+                }
             }
-        }else {
-            System.out.println("retry");
         }
     }
-}
